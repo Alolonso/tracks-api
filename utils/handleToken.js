8ddbcea4ce6b8ca19d885b0193963f5fe5d6305c
Alolonso <alonsodr11@gmail.com>
2024-07-20 15:05:03 -0600

    Implement JWT token signing and verification functions

---
+++ utils/handleToken.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken')
+
+const tokenSign = async (user) => {
+  return jwt.sign(
+    {
+      _id: user._id,
+      role: user.role,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: '2h',
+    }
+  )
+}
+
+const verifyToken = async (token) => {
+  try {
+    return jwt.verify(token, process.env.JWT_SECRET)
+  } catch (error) {
+    console.log(error)
+    return null
+  }
+}
+
+module.exports = { tokenSign, verifyToken }
